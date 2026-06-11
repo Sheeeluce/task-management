c534402510f34e4b7dffe6fcdaa416515bc13467
Lucy <lucymwago9@gmail.com>
2026-06-11 15:18:46 +0300

    validations

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -2,25 +2,18 @@ import re
 from datetime import datetime
 
 def validate_task_title(title):
-    if not isinstance(title, str):
-        return False, "Title must be string"
-    if len(title.strip()) == 0:
-        return False, "Title cannot be empty"
-    if len(title) < 3:
-        return False, "Title too short"
-    return True, "Valid"
+    if len(title) == 0:
+        return False
+    return True
     
 def validate_task_description(description):
-    if not isinstance(description, str):
-        return False, "Description must be a string"
-    if len(description.strip()) == 0:
-        return False, "Description can't be empty"
-    if len(description) < 5:
-        return False, "Description too short"
+    if len(description) == 0:
+        return False
+    return True
     
 def validate_due_date(due_date):  
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
-        return True, "Valid"
+        return True
     except ValueError:
-        return False, "Invalid date format. Use YYY-MM-DD"
+        return False
